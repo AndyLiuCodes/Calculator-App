d3087c5151adefa3533bd7b2c58b23e554c3b965
Andy Liu <ala158@sfu.ca>
2019-09-22 14:40:57 -0700

    added a new row functionality

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,23 +1,24 @@
-var a1Weight = document.getElementById("weightA1");
-var a1Score = document.getElementById("scoredA1");
-var a1Total = document.getElementById("totalA1");
+let a1Weight = document.getElementById("weightA1");
+let a1Score = document.getElementById("scoredA1");
+let a1Total = document.getElementById("totalA1");
 
-var a2Weight = document.getElementById("weightA2");
-var a2Score = document.getElementById("scoredA2");
-var a2Total = document.getElementById("totalA2");
+let a2Weight = document.getElementById("weightA2");
+let a2Score = document.getElementById("scoredA2");
+let a2Total = document.getElementById("totalA2");
 
-var a3Weight = document.getElementById("weightA3");
-var a3Score = document.getElementById("scoredA3");
-var a3Total = document.getElementById("totalA3");
+let a3Weight = document.getElementById("weightA3");
+let a3Score = document.getElementById("scoredA3");
+let a3Total = document.getElementById("totalA3");
 
-var a4Weight = document.getElementById("weightA4");
-var a4Score = document.getElementById("scoredA4");
-var a4Total = document.getElementById("totalA4");
+let a4Weight = document.getElementById("weightA4");
+let a4Score = document.getElementById("scoredA4");
+let a4Total = document.getElementById("totalA4");
 
-var meanCalc = document.getElementById("mean");
-var weightCalc = document.getElementById("weighted");
+let meanCalc = document.getElementById("mean");
+let weightCalc = document.getElementById("weighted");
+let newRow = document.getElementById("rowAdd");
 
-var numActivities = 4;
+let numActivities = 4;
 
 meanCalc.addEventListener("click", function(){
     let percentageTotal = 0;
@@ -33,7 +34,7 @@ meanCalc.addEventListener("click", function(){
     
     let percentageAvg = percentageTotal/numActivitiesFilled;
 
-    document.getElementById("result").innerHTML = `Your mean grade is: ${percentageAvg}/100`;
+    document.getElementById("result").innerHTML = `Your mean grade is: ${percentageAvg.toFixed(2)}/100`;
 });
 
 weightCalc.addEventListener("click", function(){
@@ -54,6 +55,7 @@ weightCalc.addEventListener("click", function(){
 
     document.getElementById("result").innerHTML = `Your weighted grade is: ${percentageAvg}/100`;
 });
+
 
 a1Score.addEventListener("input", function(){
     let percent = document.getElementById("percent1");
@@ -94,3 +96,50 @@ a4Total.addEventListener("input", function(){
     let percent = document.getElementById("percent4");
     percent.innerHTML = `${(parseFloat(a4Score.value)/parseFloat(a4Total.value)*100).toFixed(2)}%`;
 });
+
+newRow.addEventListener("click", function(){
+    numActivities += 1;
+    let currentIteration = numActivities;
+    let table = document.getElementById("contentTable");
+    let tableRow = document.createElement("tr");
+    tableRow.setAttribute("id", `r${numActivities}`)
+    tableRow.innerHTML += `<td>Activity ${numActivities}</td>`;
+    tableRow.innerHTML += `<td>A${numActivities}</td>`;
+    tableRow.innerHTML += `<td><input class="input" id="weightA${numActivities}"></td>`;
+    tableRow.innerHTML += `<td class="slash"><input class="input" id="scoredA${numActivities}">/<br><input class="input" id="totalA${numActivities}"></td>`;
+    tableRow.innerHTML += `<td id="percent${numActivities}"></td>`;
+    table.appendChild(tableRow);
+
+    let score = document.getElementById(`scoredA${numActivities}`);
+    let total = document.getElementById(`totalA${numActivities}`);
+
+    score.addEventListener("input", function(){
+        let percent = document.getElementById(`percent${currentIteration}`);
+        let grade = (parseFloat(score.value)/parseFloat(total.value)*100).toFixed(2);
+        if (score.value == "" && total.value =="" ){
+            percent.innerHTML = "";
+        }
+        else{
+            percent.innerHTML = `${grade}`;
+        }
+    });
+    
+    total.addEventListener("input", function(){
+        let percent = document.getElementById(`percent${currentIteration}`);
+        let grade = (parseFloat(score.value)/parseFloat(total.value)*100).toFixed(2);
+        if (score.value == "" && total.value =="" ){
+            percent.innerHTML = "";
+        }
+        else{
+            percent.innerHTML = `${grade}`;
+        }
+        
+    });
+
+    let objDiv = document.getElementById("scrollWrap");
+    objDiv.scrollTop = objDiv.scrollHeight;
+});
+
+
+
+
