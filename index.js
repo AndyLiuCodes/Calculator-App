ad5d24ab21bf7f45798adfd1543ed7abcd730be3
AndyLiuCodes <andyliu5898@gmail.com>
2019-09-24 20:55:51 -0700

    deployed

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5001
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-
-  .get('/', (req, res) => res.render('index.html'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
-  
-showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
-}
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req,res) => res.render('pages/index'))
+app.listen(PORT ,() => console.log(`Listening on ${ PORT }`))
